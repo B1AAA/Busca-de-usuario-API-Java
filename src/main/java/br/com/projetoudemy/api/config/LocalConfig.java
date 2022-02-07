0c1b7a79d6b8c5f02d2c46d2b61e18aaf81405b5
B1AAA <beatriz.soares358@gmail.com>
2022-02-07 12:09:44 -0300

    perfil de testes

---
+++ src/main/java/br/com/projetoudemy/api/config/LocalConfig.java
@@ -0,0 +1,27 @@
+package br.com.projetoudemy.api.config;
+
+import br.com.projetoudemy.api.domain.User;
+import br.com.projetoudemy.api.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.List;
+
+@Configuration
+@Profile("local")
+public class LocalConfig {
+
+    @Autowired
+    private UserRepository repository;
+
+
+    @Bean
+    public void startDb(){
+        User u1 = new User(null,"Beatriz","beatriz@gmail.com","@123");
+        User u2 = new User(null,"Maria","maria@gmail.com","@123");
+
+        repository.saveAll(List.of(u1,u2));
+    }
+}
