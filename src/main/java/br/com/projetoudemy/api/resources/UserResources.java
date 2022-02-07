2bda68367695ecee8b6b28a5ff6f52cec9169a39
B1AAA <beatriz.soares358@gmail.com>
2022-02-07 11:44:14 -0300

    classe user resource findById

--- src/main/java/br/com/projetoudemy/api/resources/UserResources.java
+++ src/main/java/br/com/projetoudemy/api/resources/UserResources.java
@@ -1,6 +1,8 @@
 package br.com.projetoudemy.api.resources;
 
 import br.com.projetoudemy.api.domain.User;
+import br.com.projetoudemy.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,11 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value="/user")
 public class UserResources {
+    @Autowired
+    private UserService service;
 
     @GetMapping(value="/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id){
 
-        return ResponseEntity.ok().body(new User(1,"Beatriz","beatriz@gmail.com","@123"));
+        return ResponseEntity.ok().body(service.findById(id));
     }
 
 
